c98a11d0c447bd1e9fa809306a0c91bde1f4a9ca
Christopher Scatliff <umscatli@myumanitoba.ca>
2018-01-28 14:34:11 -0600

    Created business package and added two classes.

---
+++ app/src/main/java/msms/comp3350/main/business/AccessUsers.java
@@ -0,0 +1,14 @@
+package msms.comp3350.main.business;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Chris on 2018-01-28.
+ */
+
+public class AccessUsers {
+
+    public ArrayList getUsers() {
+        return new ArrayList();
+    }
+}
