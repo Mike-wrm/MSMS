01ee91cf21d0f3fd3765a6f5119f8bb653b2020c
Christopher Scatliff <umscatli@myumanitoba.ca>
2018-02-03 18:48:40 -0600

    Removed other business class from wrong directory.

--- app/src/main/java/msms/comp3350/business/AccessMovies.java
+++ app/src/main/java/msms/comp3350/business/AccessMovies.java
@@ -1,8 +1,14 @@
 package msms.comp3350.business;
 
+import java.util.ArrayList;
+
 /**
  * Created by Chris on 2018-02-03.
  */
 
 public class AccessMovies {
+
+    public ArrayList getMovies() {
+        return new ArrayList();
+    }
 }
