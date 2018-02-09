4269b806cfe44e4f83fdee84bd882cb0fc64a160
ghjffbali <balij@myumanitoba.ca>
2018-02-08 18:08:24 -0600

    UserList and MovieList Changes
    Adapted SRSYS to current/expected userlist and movielist code for presentation

--- app/src/main/java/msms/comp3350/main/presentation/MovieListActivity.java
+++ app/src/main/java/msms/comp3350/main/presentation/MovieListActivity.java
@@ -16,30 +16,34 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 import msms.comp3350.main.R;
+//import msms.comp3350.main.business.AccessMovies; --> something like this
+//import msms.comp3350.main.objects.Movie;
 
 public class MovieListActivity extends Activity {
 
     // TODO change this to our "Movie" type later
-    private ArrayList<String> courseList;
-    private ArrayAdapter<String> courseArrayAdapter;
+    private ArrayList<Movie> movieList;
+    private ArrayAdapter<Movie> movieArrayAdapter;
     private int selectedMoviePosition = -1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
 
-        courseList = new ArrayList<String>();
-        // TODO change this to the method that will grab the list of movies
+        movieList = new ArrayList<Movie>();
+        // TODO change this to the method that will grab the list of movies from BUSINESS
         String result = null;
         for (int i = 0; i < 20; i++){
-            courseList.add("Movie Title " + i);
+            movieList.add("Movie Title " + i);
         }
+
         // Perform error handling on getting the list of movies
         if (result != null) {
             Messages.fatalError(this, result);
         } else {
-            courseArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_2, android.R.id.text1, courseList) {
+            movieArrayAdapter = new ArrayAdapter<Movie>(this, android.R.layout.simple_list_item_activated_2, android.R.id.text1, movieList) {
                 @Override
                 public View getView(int position, View convertView, ViewGroup parent) {
                     View view = super.getView(position, convertView, parent);
@@ -47,14 +51,14 @@ public class MovieListActivity extends Activity {
                     TextView text1 = (TextView) view.findViewById(android.R.id.text1);
 
                     // TODO change this to corresponding "Get Movie Title" method from our "Movie" class
-                    text1.setText(courseList.get(position));
+                    text1.setText(movieList.get(position));
 
                     return view;
                 }
             };
 
             final ListView listView = (ListView) findViewById(R.id.listMovies);
-            listView.setAdapter(courseArrayAdapter);
+            listView.setAdapter(movieArrayAdapter);
 
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
