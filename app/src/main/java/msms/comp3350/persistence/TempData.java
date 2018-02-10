088e7fe63db20994157e4b110b4db7185b26db4a
Christopher Scatliff <umscatli@myumanitoba.ca>
2018-02-09 20:17:31 -0600

    Added insert/update/delete methods.

--- app/src/main/java/msms/comp3350/persistence/TempData.java
+++ app/src/main/java/msms/comp3350/persistence/TempData.java
@@ -11,47 +11,107 @@ import msms.comp3350.objects.User;
 
 public class TempData {
 
-    private ArrayList<Movie> allMovies;
-    private ArrayList<User> allUsers;
+    private ArrayList<Movie> movies;
+    private ArrayList<User> users;
 
     public TempData() {
         Movie movie;
         User user;
-        allMovies = new ArrayList<Movie>();
-        allUsers = new ArrayList<User>();
+        movies = new ArrayList<Movie>();
+        users = new ArrayList<User>();
 
         movie = new Movie("The Shawshank Redemption", 1994, 9, "DRAMA", 1, 1, 2021, "ipsum lorem");
-        allMovies.add(movie);
+        movies.add(movie);
         movie = new Movie("The Lord of the Rings", 2001, 8, "FANTASY", 2, 2, 2022, "ipsum lorem");
-        allMovies.add(movie);
+        movies.add(movie);
         movie = new Movie("A Nightmare on Elm Street", 1984, 7, "HORROR", 3, 3, 2023, "ipsum lorem");
-        allMovies.add(movie);
+        movies.add(movie);
         movie = new Movie("Raiders of the Lost Ark", 1981, 6, "ACTION", 4, 4, 2024, "ipsum lorem");
-        allMovies.add(movie);
+        movies.add(movie);
         movie = new Movie("A Fish Called Wanda", 1988, 5, "COMEDY", 5, 5, 2021, "ipsum lorem");
-        allMovies.add(movie);
+        movies.add(movie);
 
         user = new User("Mike McMahon", "anime4life", 21, "MALE", 1, 1, 2021);
-        allUsers.add(user);
+        users.add(user);
         user = new User("Chris Scatliff", "getoffmylawn", 82, "MALE", 2, 2, 2022);
-        allUsers.add(user);
+        users.add(user);
         user = new User("Andrew Kozar", "iheartmybeard", 23, "MALE", 3, 3, 2023);
-        allUsers.add(user);
+        users.add(user);
         user = new User("Alex Carriere", "supertester", 24, "MALE", 4, 4, 2024);
-        allUsers.add(user);
+        users.add(user);
         user = new User("Jaivir Bali", "walmartisevil", 25, "MALE", 5, 5, 2025);
-        allUsers.add(user);
+        users.add(user);
         user = new User("Diana Prince", "wonderwoman", 86, "FEMALE", 6, 6, 2026);
-        allUsers.add(user);
+        users.add(user);
     }
 
-    public String getMoviesAll(ArrayList<Movie> movies) {
-        movies.addAll(allMovies);
+    public String getMoviesAll(ArrayList<Movie> currentMovies) {
+        currentMovies.addAll(movies);
         return null;
     }
 
-    public String getUsersAll(ArrayList<User> users) {
-        users.addAll(allUsers);
+    public String insertMovie(Movie currentMovie) {
+        int index = movies.indexOf(currentMovie);
+        if(index != -1) {
+            return "'" + currentMovie.getTitle() + "' is already added.";
+        } else {
+            movies.add(currentMovie);
+        }
+        return null;
+    }
+
+    public String updateMovie(Movie currentMovie) {
+        int index = movies.indexOf(currentMovie);
+        if(index == -1) {
+            return "'" + currentMovie.getTitle() + "' cannot be found.";
+        } else {
+            movies.set(index, currentMovie);
+        }
+        return null;
+    }
+
+    public String deleteMovie(Movie currentMovie) {
+        int index = movies.indexOf(currentMovie);
+        if(index == -1) {
+            return "'" + currentMovie.getTitle() + "' cannot be found.";
+        } else {
+            movies.remove(index);
+        }
+        return null;
+    }
+
+    public String getUsersAll(ArrayList<User> currentUsers) {
+        currentUsers.addAll(users);
+        return null;
+    }
+
+    public String insertUser(User currentUser) {
+        int index = users.indexOf(currentUser);
+        if(index != -1) {
+            return "'" + currentUser.getName() + "' is already added.";
+        } else {
+            users.add(currentUser);
+        }
+        return null;
+    }
+
+    public String updateUser(User currentUser) {
+        int index = users.indexOf(currentUser);
+        if(index == -1) {
+            return "'" + currentUser.getName() + "' cannot be found.";
+        } else {
+            users.set(index, currentUser);
+        }
+        return null;
+    }
+
+    public String deleteUser(User currentUser) {
+        int index = users.indexOf(currentUser);
+        if(index == -1) {
+            return "'" + currentUser.getName() + "' cannot be found.";
+        } else {
+            movies.remove(index);
+        }
         return null;
     }
 }
