c182283492c1bbf019899c8ac002bdcd9fab6264
alex <carrie26@myumanitoba.ca>
2018-03-11 05:12:21 -0500

    Reformatted the stub to have the same data as the database

--- app/src/main/java/msms/comp3350/persistence/TempData.java
+++ app/src/main/java/msms/comp3350/persistence/TempData.java
@@ -31,57 +31,121 @@ public class TempData implements DataAccessor
     {
         Movie movie;
         User user;
-        ArrayList<String> drama = new ArrayList<>();
-        drama.add("Drama");
-        ArrayList<String> fantasy = new ArrayList<>();
-        fantasy.add("Fantasy");
-        ArrayList<String> horror = new ArrayList<>();
-        horror.add("Horror");
-        ArrayList<String> action = new ArrayList<>();
-        action.add("Action");
-        ArrayList<String> comedy = new ArrayList<>();
-        comedy.add("Comedy");
+        ArrayList<String> movie1 = new ArrayList<>();
+        movie1.add("Comedy");
 
-        Calendar date1 = Calendar.getInstance(); // Obviously, each object  should get their own date object
-        date1.set(2021, 1, 1);  // But for the sake of my time and sanity, I'm making them share
+        ArrayList<String> movie2 = new ArrayList<>();
+        movie2.add("Comedy");
+
+        ArrayList<String> movie3 = new ArrayList<>();
+        movie3.add("Comedy");
+        movie3.add("Family");
+
+        ArrayList<String> movie4 = new ArrayList<>();
+        movie4.add("Comedy");
+        movie4.add("Family");
+
+        ArrayList<String> movie5 = new ArrayList<>();
+        movie5.add("Horror");
+
+        ArrayList<String> movie6 = new ArrayList<>();
+        movie6.add("Horror");
+
+        ArrayList<String> movie7 = new ArrayList<>();
+        movie7.add("Action");
+        movie7.add("Trending");
+
+        ArrayList<String> movie8 = new ArrayList<>();
+        movie8.add("Action");
+        movie8.add("Recent");
+
+        ArrayList<String> movie9 = new ArrayList<>();
+        movie9.add("Action");
+        movie9.add("Trending");
+
+        Calendar date1 = Calendar.getInstance(); // January = 0, December = 11.
+        date1.set(2018, 4, 3);
         Calendar date2 = Calendar.getInstance();
-        date2.set(2021, 2, 2);
+        date2.set(2018, 6, 23);
         Calendar date3 = Calendar.getInstance();
-        date3.set(2021, 3, 3);
+        date3.set(2018, 3, 11);
         Calendar date4 = Calendar.getInstance();
-        date4.set(2021, 4, 4);
+        date4.set(2019, 6, 7);
         Calendar date5 = Calendar.getInstance();
-        date5.set(2021, 5, 5);
+        date5.set(2021, 4, 1);
         Calendar date6 = Calendar.getInstance();
-        date6.set(2021, 6, 6);
+        date6.set(2018, 6, 23);
+        Calendar date7 = Calendar.getInstance();
+        date7.set(2018, 11, 31);
+        Calendar date8 = Calendar.getInstance();
+        date8.set(2019, 11, 31);
+        Calendar date9 = Calendar.getInstance();
+        date9.set(2020, 10, 1);
+
+        Calendar userDate1 = Calendar.getInstance();
+        userDate1.set(2018,3,30);
+        Calendar userDate2 = Calendar.getInstance();
+        userDate2.set(2020,11,31);
+
 
         movies = new ArrayList<Movie>();
         users = new ArrayList<User>();
 
 
-        movie = new Movie("The Shawshank Redemption", 1994, 9, drama, date1, "ipsum lorem");
-        movies.add(movie);
-        movie = new Movie("The Lord of the Rings", 2001, 8, fantasy, date2, "ipsum lorem");
-        movies.add(movie);
-        movie = new Movie("A Nightmare on Elm Street", 1984, 7, horror, date3, "ipsum lorem");
-        movies.add(movie);
-        movie = new Movie("Raiders of the Lost Ark", 1981, 6, action, date4, "ipsum lorem");
-        movies.add(movie);
-        movie = new Movie("A Fish Called Wanda", 1988, 5, comedy, date5, "ipsum lorem");
+        movie = new Movie("South Park: Bigger, Longer & Uncut", 1999, 7, movie1, date1, "Ipsum Lorem...");
+        movie.setmID(111);
         movies.add(movie);
 
-        user = new User(1, "Mike McMahon", "anime4life", 21, 'm', date1);
+        movie = new Movie("Eddie Murphy: Raw", 1987, 5, movie2, date2, "Ipsum Lorem...");
+        movie.setmID(222);
+        movies.add(movie);
+
+        movie = new Movie("Toy Story", 1995, 10, movie3, date3, "Ipsum Lorem...");
+        movie.setmID(333);
+        movies.add(movie);
+
+        movie = new Movie("Shrek", 2001, 8, movie4, date4, "Ipsum Lorem...");
+        movie.setmID(444);
+        movies.add(movie);
+
+        movie = new Movie("Friday the 13th", 2009, 3, movie5, date5, "Ipsum Lorem...");
+        movie.setmID(555);
+        movies.add(movie);
+
+        movie = new Movie("The Ring", 2002, 6, movie6, date6, "Ipsum Lorem...");
+        movie.setmID(666);
+        movies.add(movie);
+
+        movie = new Movie("Mission Impossible: Rogue Nation", 2015, 8, movie7, date7, "Ipsum Lorem...");
+        movie.setmID(777);
+        movies.add(movie);
+
+        movie = new Movie("Transformers: The Last Knight", 2017, 2, movie8, date8, "Ipsum Lorem...");
+        movie.setmID(888);
+        movies.add(movie);
+
+        movie = new Movie("Terminator 2: Judgement Day", 1991, 8, movie9, date9, "Ipsum Lorem...");
+        movie.setmID(999);
+        movies.add(movie);
+
+        user = new User(111, "Miggles", "anime4life", 21, 'M', userDate1);
         users.add(user);
-        user = new User(2, "Chris Scatliff", "getoffmylawn", 82, 'm', date2);
+
+        user = new User(222, "Smoo", "getoffmylawn", 32, 'M', userDate1);
         users.add(user);
-        user = new User(3, "Andrew Kozar", "iheartmybeard", 23, 'm', date3);
+
+        user = new User(333, "Andrew_Sempai", "iheartmybeard", 23, 'M', userDate1);
         users.add(user);
-        user = new User(4, "Alex Carriere", "supertester", 24, 'm', date4);
+
+        user = new User(444, "TestBoi", "supertester", 24, 'M', userDate1);
         users.add(user);
-        user = new User(5, "Jaivir Bali", "walmartisevil", 25, 'm', date5);
+
+        user = new User(555, "JiffyPB", "walmartisevil", 21, 'M', userDate1);
         users.add(user);
-        user = new User(6, "Diana Prince", "wonderwoman", 86, 'f', date6);
+
+        user = new User(666, "Wonder_Woman", "DCU", 82, 'F', userDate2);
         users.add(user);
+
     }
 
     public void close()
